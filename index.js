f52b5c2c3976a5fbfa11e2bc9bfa47b437809581
Derek Wan <d.wan@icloud.com>
2023-05-19 14:47:30 +0800

    Build password generator (#2)

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const getRndCharacter = () => {
+  return characters[getRndInteger(0, characters.length - 1)];
+};
+
+const getRndPassword = () => {
+  const length = getRndInteger(10, 20);
+  const chars = new Array(length).fill(null).map(getRndCharacter);
+  return chars.join("");
+};
+
+const generate = () => {
+  const [pass1, pass2] = new Array(2).fill(null).map(getRndPassword);
+  document.getElementById("output1").textContent = pass1;
+  document.getElementById("output2").textContent = pass2;
+};
