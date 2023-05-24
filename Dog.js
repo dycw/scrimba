56941386554993e2ecc3518905056f45aa2edb6d
Derek Wan <d.wan@icloud.com>
2023-05-25 00:42:40 +0800

    Tindog (#10)

---
+++ Dog.js
@@ -0,0 +1,13 @@
+// Create the Dog class here
+class Dog {
+  constructor({ name, avatar, age, bio, hasBeenSwiped, hasBeenLiked }) {
+    this.name = name;
+    this.avatar = avatar;
+    this.age = age;
+    this.bio = bio;
+    this.hasBeenSwiped = hasBeenSwiped;
+    this.hasBeenLiked = hasBeenLiked;
+  }
+}
+
+export { Dog };
